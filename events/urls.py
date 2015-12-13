49e92e11d181e461814d8da04ea892fe0230b759
vitiv <nikita.utiu@gmail.com>
2015-12-13 04:02:56 +0200

    sadasfasfaf

--- events/urls.py
+++ events/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.EventView.as_view(), name='detail'),
     url(r'^add$', views.add_event, name='add'),
     url(r'^logout', views.logout, name='logout'),
-    url(r'^login', views.login, name='login'),
+    url(r'^search', views.search, name='search'),
 ]
